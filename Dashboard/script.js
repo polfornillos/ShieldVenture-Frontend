101d54e6e2d95ff3658463485bfa8e404baeeec5
Paul Fornillos <paul.fornillosb@gmail.com>
2024-02-20 18:57:22 +0800

    Separated script and style to different files, Fixed styles

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -1,20 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
   var sidebar = document.getElementById("sidebar");
   var sidebarCollapse = document.getElementById("sidebarCollapse");
-  var rootStyles = getComputedStyle(document.documentElement);
+  var content = document.getElementById("content");
 
-  sidebarCollapse.addEventListener("click", function () {
-    var sidebarWidth = rootStyles.getPropertyValue("--sidebar-width").trim();
-    // Ensure sidebarWidth is a number to use in calculations
-    sidebarWidth = parseInt(sidebarWidth, 10);
+  // Function to handle the toggle of the sidebar
+  function toggleSidebar() {
+    sidebar.classList.toggle("active");
+    if (window.innerWidth <= 768) {
+      content.classList.toggle("active");
+    }
+  }
 
-    // Toggle the sidebar on and off
-    if (sidebar.classList.contains("active")) {
-      sidebar.style.transform = `translateX(-${sidebarWidth}px)`; // Sidebar slides out
-      sidebar.classList.remove("active");
-    } else {
-      sidebar.style.transform = "translateX(0px)"; // Sidebar slides in
-      sidebar.classList.add("active");
+  // Event listener for sidebar toggle button
+  sidebarCollapse.addEventListener("click", toggleSidebar);
+
+  // Event listener for window resize
+  window.addEventListener("resize", function () {
+    // If window is resized to desktop view and sidebar is hidden, show it
+    if (window.innerWidth > 768 && sidebar.classList.contains("active")) {
+      toggleSidebar(); // This will remove 'active' class and show the sidebar
     }
   });
 });
